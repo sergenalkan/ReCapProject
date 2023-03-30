c5e6d94214f76ba1762cd863b183b76f17b14549
Sergen Alkan <sergen_alkan@hotmail.com>
2023-03-30 05:49:33 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using DataAccess.Abstract;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            Car car1 = new Car();
+            car1.CarId = 4;
+            car1.BrandId = 2;
+            car1.ColorId = 3;
+            car1.DailyPrice = 750000;
+            car1.Description = "Clio Icon Paket";
+            car1.ModelYear = 2023;
+            carManager.Add(car1);
+            Console.WriteLine(car1.Description+" açıklamalı araç eklendi\n------------------");
+            
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.BrandId+" markalı aracın açıklaması:"+car.Description);
+            }
+
+            Console.WriteLine("-------------------");
+            foreach (var car in carManager.GetByID(3))
+            {
+                Console.WriteLine(car.CarId + ":Id Numaralı Araç" + car.Description);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,64 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId=1,BrandId=1,ColorId=1,ModelYear=2000,DailyPrice=1000,Description="benim araba" },
+                new Car{CarId=2,BrandId=1,ColorId=2,ModelYear=2010,DailyPrice=20000,Description="astra hasar" },
+                new Car{CarId=3,BrandId=2,ColorId=2,ModelYear=2015,DailyPrice=100000,Description="az hasar" },
+                new Car{CarId=4,BrandId=3,ColorId=3,ModelYear=2018,DailyPrice=500000,Description="otomatik" },
+                new Car{CarId=5,BrandId=3,ColorId=3,ModelYear=2023,DailyPrice=800000,Description="elektrikli" },
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c=>c.CarId==car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+        public List<Car> GetById(int id)
+        {
+            return _cars.Where(c => c.CarId == id).ToList();
+        }
+        public List<Car> GetAllByBrand(int brandId)
+        {
+            return _cars.Where(c => c.BrandId == brandId).ToList();
+        }
+
+        public List<Car> GetAllByColor(int colorId)
+        {
+            return _cars.Where(c => c.ColorId == colorId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
