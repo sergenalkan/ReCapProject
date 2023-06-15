17bde0b63baf873f840644e7cb66016f14d69246
Sergen Alkan <sergen_alkan@hotmail.com>
2023-06-15 06:30:07 +0300

    15.06.2023 Car Image İşlemleri yapıldı.

---
+++ Business/Abstract/ICarImageService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Utilities.Result;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ICarImageService
+    {
+        //IFormFile için using Microsoft.AspNetCore.Http; kütüphanesini sağ tık install latest versiyon seç.
+        IResult Add(IFormFile file, CarImage carImage);
+        IResult Delete(CarImage carImage);
+        IResult Update(IFormFile file, CarImage carImage);
+
+        IDataResult<List<CarImage>> GetAll();
+        IDataResult<List<CarImage>> GetByCarId(int carId);
+        IDataResult<CarImage> GetByImageId(int imageId);
+    }
+}
